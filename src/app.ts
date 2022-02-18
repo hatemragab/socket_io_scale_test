71d07225c9dd21a47a7ce7ee6dad10019ee4653b
hatemragab <hatemragap5@gmail.com>
2022-02-18 14:08:32 +0200

    remove unnecessary packages

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import {express} from "express-useragent";
 import {Server} from "socket.io";
 import http from 'http';
 import ChatServer from "./socket_io/chat_server";
-
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
 declare global {
     namespace NodeJS {
         interface Global {
